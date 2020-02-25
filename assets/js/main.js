2d2aa396d788263513d66b09ffa80d3c37b96eb6
Matthew Weaber <matthew.weaber@ciirus.com>
2020-02-25 12:37:21 -0500

    Saving work to a repo

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// function themeSwap() {
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+//     console.log('theme-swapper');
+//  }
+
+// themeSwap();
+
+
+
+
+
